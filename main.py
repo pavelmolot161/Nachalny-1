634d9cb5287d1cfaef9175a35de2e1cb896ea048
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-06-23 07:07:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+a = 'arbuz'
+d = len(a)
+print(d)
+
+a = 'arbuz'
+print(len(a))
+
+print(6 + 9)
+print(6 - 9)
+
+a = [9,6,21]
+print(sum(a) / len(a))
+
+print('Понедельник')
+print(type('Вторник'))
+print('Понедельник,' + ' Вторник')
+
+a = 9
+b = 5
+c = 7
+f = (((a * b) + (a * c)) ** 3) / 2
+print(f)
