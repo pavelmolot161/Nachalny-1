85db8ebd3acac9a99513fcdf3975e27a6eec94eb
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-06-23 14:37:02 +0300

    Add files via upload

---
+++ mlt4.py
@@ -0,0 +1,8 @@
+first_name = "Python,"
+second_name = "всего задач:"
+apple_count = "12,"
+seuu_name = "затрачено часов:"
+orang_count = "1.5,"
+seuuu_name = "среднее время выполнения"
+orannn_count = "0.125"
+print('Курс:',first_name, second_name, apple_count, seuu_name,orang_count, seuuu_name, orannn_count,'часа.')
